8fa2eeb8dd321e84e4c8f5c971e99a9789ef19bd
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-09-20 17:12:37 +0300

    Fixed paths

--- src/main/java/com/minakov/railwayticketbooking/io/FilePaths.java
+++ src/main/java/com/minakov/railwayticketbooking/io/FilePaths.java
@@ -4,12 +4,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public enum FilePaths {
-    CRUISES(Paths.get("src", "resources", "cruises")),
-    ROUTES(Paths.get("src", "resources", "routes")),
-    STATIONS(Paths.get("src", "resources", "stations")),
-    TICKETS(Paths.get("src", "resources", "tickets")),
-    TRAINS(Paths.get("src", "resources", "trains")),
-    WAGONS(Paths.get("src", "resources", "wagons"));
+    CRUISES(Paths.get("src", "resources", "cruises.txt")),
+    ROUTES(Paths.get("src", "resources", "routes.txt")),
+    STATIONS(Paths.get("src", "resources", "stations.txt")),
+    TICKETS(Paths.get("src", "resources", "tickets.txt")),
+    TRAINS(Paths.get("src", "resources", "trains.txt")),
+    WAGONS(Paths.get("src", "resources", "wagons.txt"));
 
     private Path path;
 
