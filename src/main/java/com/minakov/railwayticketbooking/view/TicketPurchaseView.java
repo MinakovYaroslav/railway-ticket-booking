f16fcf0786544d38886ae9f30affd4fe904e121e
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-10-22 00:29:08 +0300

    Added userController value; Updated constructor; Updated content() method;

--- src/main/java/com/minakov/railwayticketbooking/view/TicketPurchaseView.java
+++ src/main/java/com/minakov/railwayticketbooking/view/TicketPurchaseView.java
@@ -2,55 +2,59 @@ package com.minakov.railwayticketbooking.view;
 
 import com.minakov.railwayticketbooking.controller.CruiseController;
 import com.minakov.railwayticketbooking.controller.TicketController;
-import com.minakov.railwayticketbooking.exception.CruiseNotFoundException;
+import com.minakov.railwayticketbooking.controller.UserController;
 import com.minakov.railwayticketbooking.exception.TicketNotFoundException;
 import com.minakov.railwayticketbooking.exception.WagonNotFoundException;
 import com.minakov.railwayticketbooking.io.Console;
-import com.minakov.railwayticketbooking.model.Cruise;
-import com.minakov.railwayticketbooking.model.Ticket;
-import com.minakov.railwayticketbooking.model.Train;
-import com.minakov.railwayticketbooking.model.WagonType;
+import com.minakov.railwayticketbooking.model.*;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.Random;
 
 import static com.minakov.railwayticketbooking.config.DateFormatConfig.dateFormat;
+import static com.minakov.railwayticketbooking.config.DateFormatConfig.userBirthdayFormat;
 
 public class TicketPurchaseView extends ViewTemplate {
 
     private CruiseController cruiseController;
     private TicketController ticketController;
+    private UserController userController;
 
     public TicketPurchaseView() {
         this.cruiseController = new CruiseController();
         this.ticketController = new TicketController();
+        this.userController = new UserController();
         content();
     }
 
     @Override
     public void content() {
+        Map<Integer, Cruise> cruises = cruiseController.cruiseTab();
         String line;
-        Long id;
+        Integer cruiseNumber;
         Cruise cruise;
         Train train;
         WagonType wagonType;
         String firstName;
         String lastName;
+        Date birthday;
+        User user;
 
-
-        cruiseController.findAll().forEach(c -> { //Show list of cruises
-            System.out.println(c.getId() + "." + " " + dateFormat.format(c.getRoute().getDepartureDate()) + " " + c.getRoute().getOrigin().getName() +
-                    " - " + c.getRoute().getDestination().getName() + " " + dateFormat.format(c.getRoute().getArrivalDate()));
+        for (Map.Entry<Integer, Cruise> entry : cruises.entrySet()) { //Show list of cruises
+            System.out.println("№" + entry.getKey() + " " + dateFormat.format(entry.getValue().getRoute().getDepartureDate()) + " " + entry.getValue().getRoute().getOrigin().getName() +
+                    " - " + entry.getValue().getRoute().getDestination().getName() + " " + dateFormat.format(entry.getValue().getRoute().getArrivalDate()));
             System.out.println("ECONOMY seat price: " + new Random().nextInt(100) + "$");
             System.out.println("BUSINESS seat price: " + new Random().nextInt(300) + "$");
             System.out.println("PREMIUM seat price: " + new Random().nextInt(500) + "$");
             System.out.println();
-        });
+        }
 
         System.out.println("Enter cruise id: ");
         line = Console.input();
-        id = Long.valueOf(line);
+        cruiseNumber = Integer.valueOf(line);
         try {
-            cruise = cruiseController.findById(id);
+            cruise = cruises.get(cruiseNumber);
             train = cruise.getTrain();
 
             System.out.println("Enter ticket type: ");
@@ -65,30 +69,41 @@ public class TicketPurchaseView extends ViewTemplate {
             line = Console.input();
             lastName = line;
 
-            Ticket ticket = new Ticket.TicketBuilder()
+            System.out.println("Enter your birthday DD-MM-YYYY : ");
+            line = Console.input();
+            birthday = userBirthdayFormat.parse(line);
+
+            user = new User.UserBuilder()
                     .setFirstName(firstName)
                     .setLastName(lastName)
+                    .setBirthday(birthday)
+                    .build();
+
+            user = userController.create(user);
+
+            Ticket ticket = new Ticket.TicketBuilder()
+                    .setUser(user)
                     .setSeatType(wagonType)
                     .setCruise(cruise)
                     .setTrain(train)
                     .build();
+
             ticket = ticketController.create(ticket);
 
             System.out.println("Purchasing complete!");
             System.out.println("Your ticket info: ");
             System.out.println("Ticket id: " + ticket.getId());
-            System.out.println("First name: " + ticket.getFirstName());
-            System.out.println("Last name: " + ticket.getLastName());
-            System.out.println(cruise.getId() + "." + " " + dateFormat.format(cruise.getRoute().getDepartureDate()) + " " + cruise.getRoute().getOrigin().getName() +
+            System.out.println("First name: " + user.getFirstName());
+            System.out.println("Last name: " + user.getLastName());
+            System.out.println(dateFormat.format(cruise.getRoute().getDepartureDate()) + " " + cruise.getRoute().getOrigin().getName() +
                     " - " + cruise.getRoute().getDestination().getName() + " " + dateFormat.format(cruise.getRoute().getArrivalDate()));
-            System.out.println("Train id: " + ticket.getTrain().getId());
-            System.out.println("Wagon id: " + ticket.getWagon().getId());
+            System.out.println("Train №: " + ticket.getTrain().getNumber());
+            System.out.println("Wagon №: " + ticket.getWagon().getPositionNumber());
             System.out.println("Seat type: " + ticket.getSeatType());
-            System.out.println("Price: " + ticket.getPrice());
+            System.out.println("Price: " + ticket.getPrice() + "$");
             System.out.println("Order date: " + dateFormat.format(ticket.getOrderDate()));
 
-
-        } catch (CruiseNotFoundException | WagonNotFoundException | Exception | TicketNotFoundException e) {
+        } catch (WagonNotFoundException | TicketNotFoundException | Exception e) {
             e.printStackTrace();
         }
     }
