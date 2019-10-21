491a24007dae58fb5b0d0a72db69fde740ebbbed
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-10-22 00:20:44 +0300

    Added UserRepository interface;

---
+++ src/main/java/com/minakov/railwayticketbooking/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.minakov.railwayticketbooking.repository;
+
+import com.minakov.railwayticketbooking.model.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends GenericRepository<User, UUID> {
+}
