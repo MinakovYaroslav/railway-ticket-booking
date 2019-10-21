ae69f6f2e15eba161bbdb9c40f7c1580c39e76c1
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-10-22 00:01:52 +0300

    Added number value; Updated constructor; Added getters;

--- src/main/java/com/minakov/railwayticketbooking/model/Train.java
+++ src/main/java/com/minakov/railwayticketbooking/model/Train.java
@@ -1,14 +1,26 @@
 package com.minakov.railwayticketbooking.model;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Train extends AbstractIdentifiable {
 
+    private int number;
+
     private List<Wagon> wagons;
 
-    public Train(Long id, List<Wagon> wagons) {
+    public Train(UUID id, int number, List<Wagon> wagons) {
         super(id);
+        this.number = number;
         this.wagons = wagons;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 
     public List<Wagon> getWagons() {
