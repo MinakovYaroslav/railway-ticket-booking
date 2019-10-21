640eb1670029327c4320566130a787efd8f6ac44
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-10-21 23:42:44 +0300

    Added userBirthdayFormat field

--- src/main/java/com/minakov/railwayticketbooking/config/DateFormatConfig.java
+++ src/main/java/com/minakov/railwayticketbooking/config/DateFormatConfig.java
@@ -6,4 +6,5 @@ import java.util.Locale;
 public class DateFormatConfig {
 
     public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault());
+    public static SimpleDateFormat userBirthdayFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
 }
