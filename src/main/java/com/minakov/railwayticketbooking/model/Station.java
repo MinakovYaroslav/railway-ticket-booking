b203232d3b017af13648fe8fcac3af930882ee5c
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-10-22 00:07:55 +0300

    Added Hibernate annotations; Updated constructor; Added default constructor;

--- src/main/java/com/minakov/railwayticketbooking/model/Station.java
+++ src/main/java/com/minakov/railwayticketbooking/model/Station.java
@@ -1,12 +1,23 @@
 package com.minakov.railwayticketbooking.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "stations")
 public class Station extends AbstractIdentifiable {
 
+    @Column(name = "name")
     private String name;
 
-    public Station(Long id, String name) {
+    public Station(UUID id, String name) {
         super(id);
         this.name = name;
+    }
+
+    public Station() {
     }
 
     public String getName() {
