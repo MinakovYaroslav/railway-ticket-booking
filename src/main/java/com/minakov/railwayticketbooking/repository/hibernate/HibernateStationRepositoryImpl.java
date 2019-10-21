ea268be6fe33fb0280e6d1d4fb1915e9f3ad5714
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-10-22 00:10:04 +0300

    Added HibernateStationRepositoryImpl class;

---
+++ src/main/java/com/minakov/railwayticketbooking/repository/hibernate/HibernateStationRepositoryImpl.java
@@ -0,0 +1,76 @@
+package com.minakov.railwayticketbooking.repository.hibernate;
+
+import com.minakov.railwayticketbooking.model.Station;
+import com.minakov.railwayticketbooking.repository.StationRepository;
+import com.minakov.railwayticketbooking.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.UUID;
+
+public class HibernateStationRepositoryImpl implements StationRepository {
+
+    @Override
+    public List<Station> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Station", Station.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // delete the station object
+            session.delete(String.valueOf(id), Station.class);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Station findById(UUID id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Station.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Station create(Station station) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the station objects
+            session.save(station);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        return station;
+    }
+
+    @Override
+    public Station update(Station station) {
+        return null;
+    }
+}
