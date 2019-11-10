54996f12de1d13c160ed3c41231b05bdec331814
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-11-10 17:50:00 +0300

    Added 5.sql file

---
+++ src/main/resources/sql/5.sql
@@ -0,0 +1,9 @@
+SELECT users.*
+FROM users, tickets, cruises, routes
+GROUP BY tickets.price, users.id, tickets.user_id, tickets.order_date, tickets.cruise_id, cruises.route_id, routes.id, routes.arrival_date, cruises.id
+HAVING users.id = tickets.user_id
+   AND tickets.cruise_id = cruises.id
+   AND cruises.route_id = routes.id
+   AND routes.arrival_date = 'Saint Petersburg'
+   AND SUM(tickets.price) >= tickets.price
+   AND tickets.order_date BETWEEN STR_TO_DATE('2019-01-01','%Y-%m-%d') AND STR_TO_DATE('2019-12-31','%Y-%m-%d')
