bf5363471b3c18481be4e28978cfc97f921f9562
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-11-10 18:42:14 +0300

    Fixed query

--- src/main/resources/sql/5.sql
+++ src/main/resources/sql/5.sql
@@ -1,10 +1,16 @@
 # Поиск пассажиров, купивших билеты в указанный пункт назначения, на сумму, больше указанной в запросе, в указанный период времени
 SELECT users.*
-FROM users, tickets, cruises, routes
-GROUP BY tickets.price, users.id, tickets.user_id, tickets.order_date, tickets.cruise_id, cruises.route_id, routes.id, routes.arrival_date, cruises.id
+FROM users,
+     tickets
+GROUP BY tickets.price, users.id, tickets.user_id, tickets.order_date, tickets.cruise_id
 HAVING users.id = tickets.user_id
-   AND tickets.cruise_id = cruises.id
-   AND cruises.route_id = routes.id
-   AND routes.arrival_date = 'Saint Petersburg'
-   AND SUM(tickets.price) >= tickets.price
-   AND tickets.order_date BETWEEN STR_TO_DATE('2019-01-01','%Y-%m-%d') AND STR_TO_DATE('2019-12-31','%Y-%m-%d')
+   AND SUM(tickets.price) > tickets.price
+   AND tickets.order_date BETWEEN STR_TO_DATE('2019-01-01', '%Y-%m-%d') AND STR_TO_DATE('2019-12-31', '%Y-%m-%d')
+   AND tickets.cruise_id =
+       (SELECT cruises1.id
+        FROM cruises cruises1,
+             routes,
+             stations
+        WHERE cruises1.route_id = routes.id
+          AND routes.destination_station_id = stations.id
+          AND stations.`name` = 'Saint Petersburg')
